e85ff126533ebdca884c4d381da64338d35c5d26
Josh Bell <joshbell@Micheles-MBP.home>
2018-09-24 00:37:58 -0400

    add ids

--- post.php
+++ post.php
@@ -38,7 +38,7 @@ body {font-family: "Lato", sans-serif}
 
 <header>
         <div class="w3-display-container w3-center">
-            <img src="/w3images/la.jpg" style="width:100%">
+            <img src="https://www.w3schools.com/w3images/la.jpg" style="width:100%">
             <div class="w3-display-bottommiddle w3-container w3-text-white w3-padding-32 w3-hide-small">
               <h3>Content</h3>
               <p><b>nostrud exercitation ullamco laboris nisi ut aliquip</b></p>   
@@ -68,9 +68,10 @@ body {font-family: "Lato", sans-serif}
 <main>
         <div class="w3-container w3-content w3-center w3-padding-64" style="max-width:800px" id="band">
         <div class="w3-container w3-white">
-                <h1 style="text-align:justify"><b>Post title</b></h1>
-                <p style="text-align:justify"class="w3-opacity">November 27, 2016</p>
-                <p class="w3-justify">Content. Praesent tincidunt sed tellus ut rutrum sed vitae justo.</p>
+                <h1 style="text-align:justify" id="title"><b>Post title</b></h1>
+                <p style="text-align:justify"class="w3-opacity" id="date">November 27, 2016</p>
+                <p style="text-align:justify"class="w3-opacity" id="author">Author Name</p>
+                <p class="w3-justify" id="content">Content. Praesent tincidunt sed tellus ut rutrum sed vitae justo.</p>
                 <p class="w3-justify">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
         </div>
         </div>
