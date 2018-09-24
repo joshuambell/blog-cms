fbf5919d37e5686f202581f0fe1434fc71dcfca9
Josh Bell <joshbell@Micheles-MBP.home>
2018-09-23 23:01:31 -0400

    added variables

---
+++ part9.php
@@ -0,0 +1,9 @@
+<?php
+    ini_set('display_errors', 1);    // Show errors for debugging
+?>
+
+$firstName = "Jane";                                //String type variable
+$lastName = "Doe";                                  //String type variable
+$age = 35;                                        //Integer type variable
+$married = true;                                  //Boolean type variable
+$childrenName = array("Alice", "Bob");             //Array type variable
