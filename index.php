11fe334083ff6614aca241c45062180bbb8294bb
Josh Bell <joshbell@Micheles-MBP.home>
2018-09-30 15:28:52 -0400

    fix problem with code

--- index.php
+++ index.php
@@ -28,17 +28,8 @@ include 'header.php';
           }
             return $postTitles;
            }
+        ?>
         
-        <ul>
-        <?php
-            $postTitles = getPostTitlesFromDatabase();
-
-            foreach($postTitles as $postTitle) {
-                echo "<li><a href='post.php?title=" . $postTitle . "'>" . $postTitle . "</a></li>";
-            }
-          ?>
-        </ul>
-
         <div class="w3-content" style="max-width:2000px;margin-top:46px">
         <div class="w3-container w3-content w3-center w3-padding-64" style="max-width:800px">
             <h2 class="w3-wide">Hello World</h2>
@@ -48,9 +39,9 @@ include 'header.php';
         <section>
         <div class="w3-row w3-padding-32">
          <h2 class="w3-wide">Posts</h2>
-              <div class="w3-third">
-             
-                <p>Title #1</p>
+            
+          <div class="w3-third">
+             <p>Title #1</p>
                 <img src="https://www.w3schools.com/w3images/bandmember.jpg" class="w3-round w3-margin-bottom" alt="Random Name" style="width:60%">
               </div>
              
@@ -63,6 +54,7 @@ include 'header.php';
                 <img src="https://www.w3schools.com/w3images/bandmember.jpg" class="w3-round" alt="Random Name" style="width:60%">
               </div>
               
+              </div>
             </div>
           </div>
         </section>
